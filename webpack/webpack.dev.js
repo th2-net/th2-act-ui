a95642346e9e37673bed8dd3cd2e39a5a09c85ba
Vekesser Vladimir <vekesser_vladimir@mail.ru>
2021-03-04 14:13:22 +0400

    [JIRA] TH2-1580 Migrate act-ui message editor to json schema

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,5 +1,5 @@
-/******************************************************************************
- * Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+/** ****************************************************************************
+ * Copyright 2020-2021 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ ***************************************************************************** */
 
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
@@ -38,7 +38,7 @@ module.exports = webpackMerge(commonConfig, {
         hot: true,
         proxy: {
             '*': {
-                target: 'http://srt022:8081',
+                target: 'http://th2-qa:31467',
                 changeOrigin: true,
                 secure: false,
             }
