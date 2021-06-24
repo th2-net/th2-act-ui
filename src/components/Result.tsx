c4193bc451d74e07a8ee74dc195107b2c93d969c
Igor Razvodov <igor.razvodov@exactprosystems.com>
2021-06-24 12:30:45 +0300

    Handle parse errors, fix event link (#12)

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -15,44 +15,69 @@
  ***************************************************************************** */
 
 import React from 'react';
-import { MessageSendingResponse, ParsedMessageSendingResponse } from '../models/Message';
+import {
+	ActSendingResponse,
+	MessageSendingResponse,
+	ParsedMessageSendingResponse,
+} from '../models/Message';
 import '../styles/result.scss';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-const Result = ({
-	response,
-}: { response: MessageSendingResponse | null }) => {
+const Result = ({ response }: { response: MessageSendingResponse | null }) => {
 	if (!response) {
-		return (
-			<div className='result' />
-		);
+		return <div className='result' />;
 	}
 
 	const { code, message } = response;
 
-	const queryParameter: string | null = (JSON.parse(message) as ParsedMessageSendingResponse).eventId;
+	const parseContent = (): { link: string | null; content: string } => {
+		try {
+			const parsedContent = JSON.parse(message);
+			const renderData = {
+				link: getLink(parsedContent),
+				content: JSON.stringify(parsedContent, null, 2),
+			};
+			return renderData;
+		} catch (error) {
+			return {
+				link: null,
+				content: message,
+			};
+		}
+	};
 
-	const rootLink = isDev
-		? 'localhost:9000'
-		: window.location.href
-			.substring(0, window.location.href.indexOf('/act-ui'));
+	const getLink = (obj: unknown): string | null => {
+		const rootLink = isDev
+			? window.location.href.slice(0, window.location.href.lastIndexOf('/'))
+			: window.location.href.substring(0, window.location.href.indexOf('/act-ui'));
+
+		let eventId: null | string = null;
+
+		try {
+			if (typeof obj === 'object' && obj !== null && 'eventId' in obj) {
+				eventId = (obj as ActSendingResponse | ParsedMessageSendingResponse).eventId;
+			}
+			return eventId ? `${rootLink}/?eventId=${eventId}` : null;
+		} catch (error) {
+			return null;
+		}
+	};
+
+	const { link, content } = parseContent();
 
 	return (
-
 		<div className={`result ${code === 200 ? 'ok' : 'error'}`}>
-			<pre className="result-value">
-				{queryParameter !== null ? (
+			<pre className='result-value'>
+				{link && (
 					<>
-						<div>
-							Message is sent successfully
-						</div>
-						<a href={`${rootLink}/?eventId=${queryParameter}`}
-						   rel="noreferrer"
-						   target="_blank">report link</a>
+						<div>Message is sent successfully</div>
+						<a href={link} rel='noreferrer' target='_blank'>
+							report link
+						</a>
 					</>
-				) : null}
-				{message}
+				)}
+				{content}
 			</pre>
 		</div>
 	);
