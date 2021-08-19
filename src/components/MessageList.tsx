5a61415013fc8c020da95e238f0c049bea7b6a5e
MariaMakii <themarymakii@gmail.com>
2021-08-19 16:32:38 +0300

    Fixed files import, fixed dictionary editor loading

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -25,407 +25,408 @@ import SplitViewPane from '../split-view/SplitViewPane';
 import { nanoid } from '../../node_modules/nanoid';
 
 export type Indicator =
-	 | 'indicator-unvisible'
-	 | 'indicator-edited'
-	 | 'indicator-successful'
-	 | 'indicator-unsuccessful';
+	| 'indicator-unvisible'
+	| 'indicator-edited'
+	| 'indicator-successful'
+	| 'indicator-unsuccessful';
 
 export type ParsedMessageItem = {
-	 sessionId: string;
-	 dictionary: string;
-	 messageType: string;
-	 message: object | string;
-	 delay: number | 0;
- };
+	sessionId: string;
+	dictionary: string;
+	messageType: string;
+	message: object | string;
+	delay: number;
+};
 
 export interface ActMessageItem {
-	 actBox: string;
-	 fullServiceName: string;
-	 methodName: string;
-	 message: object | string;
-	 delay: number | 0;
- }
+	actBox: string;
+	fullServiceName: string;
+	methodName: string;
+	message: object | string;
+	delay: number;
+}
 
- interface MessageCardControlsProps {
-	 deleteMessage: Function;
-	 editMessageMode: boolean;
-	 index: number;
-	 indicator: Indicator;
- }
-
- interface EditMessageProps {
-	 editMessageMode: boolean;
-	 editedMessageIndex: number;
-	 indicators: Indicator[];
- }
-
- interface MessageItemProps extends EditMessageProps {
-	 index: number;
-	 message: ParsedMessageItem | ActMessageItem;
-	 selectMessage: Function;
-	 setDelay: Function;
-	 deleteMessage: Function;
- }
-
- interface MessageListProps extends EditMessageProps {
-	 messages: ParsedMessageItem[] | ActMessageItem[];
- }
-
- interface MessageEditAreaProps extends EditMessageProps {
-	 messages: ParsedMessageItem[] | ActMessageItem[];
-	 eventsPanelArea: number;
- }
+interface EditMessageProps {
+	editMessageMode: boolean;
+	editedMessageIndex: number;
+	indicators: Indicator[];
+}
 
 export const Messages = () => {
-	 const store = useStore();
+	const store = useStore();
 
-	 const downloadFile = (content: string, filename: string, extension: string) => {
-		 const file = new Blob([content], { type: extension });
+	const downloadFile = (content: string, filename: string, extension: string) => {
+		const file = new Blob([content], { type: extension });
 
-		 if (window.navigator.msSaveOrOpenBlob) {
-			 window.navigator.msSaveOrOpenBlob(file);
-		 } else {
-			 const a = document.createElement('a');
-			 const url = URL.createObjectURL(file);
-			 a.href = url;
-			 a.download = filename;
-			 document.body.appendChild(a);
-			 a.click();
-			 document.body.removeChild(a);
-			 window.URL.revokeObjectURL(url);
-		 }
-	 };
+		if (window.navigator.msSaveOrOpenBlob) {
+			window.navigator.msSaveOrOpenBlob(file);
+		} else {
+			const a = document.createElement('a');
+			const url = URL.createObjectURL(file);
+			a.href = url;
+			a.download = filename;
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+			window.URL.revokeObjectURL(url);
+		}
+	};
 
-	 const loadFromFile = (file: FileList | null) => {
-		 if (file != null) {
-			 const reader = new FileReader();
-			 reader.readAsText(file.item(0) as Blob);
-			 reader.onload = () => {
-				 jsonMessagesFromString(reader.result);
-			 };
-		 }
-	 };
+	const loadFromFile = (file: FileList | null) => {
+		if (file != null) {
+			const reader = new FileReader();
+			reader.readAsText(file.item(0) as Blob);
+			reader.onload = () => {
+				jsonMessagesFromString(reader.result);
+			};
+		}
+	};
 
-	 const jsonMessagesFromString = (rawFromFile: string | ArrayBuffer | null) => {
-		 try {
-			 const json = JSON.parse(rawFromFile as string);
-			 store.clearParsedMessages();
+	const jsonMessagesFromString = (rawFromFile: string | ArrayBuffer | null) => {
+		try {
+			const json = JSON.parse(rawFromFile as string);
+			store.clearParsedMessages();
 
-			 for (let i = 0; i < json.length; i++) {
-				 json[i].delay = 0;
-				 store.addParsedMessage(json[i]);
-			 }
-		 } catch (Error) {
-			 // eslint-disable-next-line no-alert
-			 alert(Error);
-		 }
-	 };
+			for (let i = 0; i < json.length; i++) {
+				json[i].delay = 0;
+				store.addParsedMessage(json[i]);
+			}
+		} catch (error) {
+			// eslint-disable-next-line no-alert
+			alert('Could not read the file. Please, try to select another file');
+		}
+	};
 
-	 const replaySendMessage = (array: ParsedMessageItem[] | ActMessageItem[], index: number) => {
-		 if (store.isReplay) {
-			 setTimeout(() => {
-				 store.sendMessage(array[index], index);
-				 // eslint-disable-next-line no-param-reassign
-				 index++;
-				 if (index === array.length) {
-					 switchReplay();
-				 } else {
-					 replaySendMessage(array, index);
-				 }
-			 }, array[index].delay);
-		 }
-	 };
+	const replaySendMessage = (array: ParsedMessageItem[] | ActMessageItem[], index: number) => {
+		if (store.isReplay) {
+			setTimeout(() => {
+				store.sendMessage(array[index], index);
+				// eslint-disable-next-line no-param-reassign
+				index++;
+				if (index === array.length) {
+					switchReplay();
+				} else {
+					replaySendMessage(array, index);
+				}
+			}, array[index].delay);
+		}
+	};
 
-	 let switchValue = false;
-	 const switchReplay = () => {
-		 switchValue = !switchValue;
-		 if (switchValue) {
-			 store.setEditMessageMode(false);
-			 store.setReplayMode(true);
-			 replaySendMessage(store.getCurrentMessagesArray(), 0);
-		 } else {
-			 store.setReplayMode(false);
-		 }
-	 };
+	let switchValue = false;
+	const switchReplay = () => {
+		switchValue = !switchValue;
+		if (switchValue) {
+			store.setEditMessageMode(false);
+			store.setReplayMode(true);
+			replaySendMessage(store.getCurrentMessagesArray(), 0);
+		} else {
+			store.setReplayMode(false);
+		}
+	};
 
-	 const exportFn = () => {
-		 downloadFile(
-			 JSON.stringify(store.getCurrentMessagesArray()),
-			 store.selectedSchemaType === 'parsed-message' ? 'parsedMessages' : 'actMessages',
-			 'application/json',
-		 );
-	 };
+	const exportFn = () => {
+		downloadFile(
+			JSON.stringify(store.getCurrentMessagesArray()),
+			store.selectedSchemaType === 'parsed-message' ? 'parsedMessages' : 'actMessages',
+			'application/json',
+		);
+	};
 
-	 return (
-		 <div>
-			 <MessageEditArea
-				 messages={store.getCurrentMessagesArray()}
-				 indicators={store.indicators.slice()}
-				 editMessageMode={store.editMessageMode}
-				 editedMessageIndex={store.editedMessageIndex}
-				 eventsPanelArea={store.eventsPanelArea}
-			 />
+	return (
+		<div>
+			<MessageEditArea
+				messages={store.getCurrentMessagesArray()}
+				indicators={store.indicators.slice()}
+				editMessageMode={store.editMessageMode}
+				editedMessageIndex={store.editedMessageIndex}
+				eventsPanelArea={store.eventsPanelArea}
+				object={store.selectedDictionaryName}
+			/>
 
-			 <div>
-				 <button
-					 disabled={store.editMessageMode}
-					 className='mainButton'
-					 onClick={store.clearParsedMessages}>
-					 Clear
-				 </button>
+			<div>
+				<button
+					disabled={store.editMessageMode}
+					className='mainButton'
+					onClick={store.clearParsedMessages}>
+					Clear
+				</button>
 
-				 <button
-					 disabled={store.getCurrentMessagesArray().length === 0}
-					 className='mainButton'
-					 onClick={exportFn}>
-					 Export
-				 </button>
+				<button
+					disabled={store.getCurrentMessagesArray().length === 0}
+					className='mainButton'
+					onClick={exportFn}>
+					Export
+				</button>
 
-				 <button
-					 disabled={store.editMessageMode}
-					 className='mainButton'
-					 onClick={switchReplay}>
-					 {store.isReplay ? 'Stop' : 'Replay'}
-				 </button>
+				<button
+					disabled={store.editMessageMode}
+					className='mainButton'
+					onClick={switchReplay}>
+					{store.isReplay ? 'Stop' : 'Replay'}
+				</button>
 
-				 <input
-					 disabled={store.editMessageMode}
-					 id='file'
-					 type='file'
-					 accept='.json'
-					 onChange={e => {
-						 if (e != null) {
-							 loadFromFile(e?.target.files);
-						 }
-					 }}
-				 />
-			 </div>
-		 </div>
-	 );
+				<input
+					disabled={store.editMessageMode}
+					value=''
+					id='file'
+					type='file'
+					accept='.json'
+					onChange={e => {
+						if (e != null) {
+							loadFromFile(e?.target.files);
+						}
+					}}
+				/>
+			</div>
+		</div>
+	);
 };
 
 const MessageEntity = (props: { message: ParsedMessageItem | ActMessageItem }) => {
-	 const store = useStore();
-	 if (store.selectedSchemaType === 'parsed-message') {
-		 return (
-			 <div className='messageEntity'>
-				 <p>
-					 <b>session: </b>
-					 {(props.message as ParsedMessageItem).sessionId}
-				 </p>
-				 <p>
-					 <b>dictionary: </b>
-					 {(props.message as ParsedMessageItem).dictionary}
-				 </p>
-				 <p>
-					 <b>messageType: </b>
-					 {(props.message as ParsedMessageItem).messageType}
-				 </p>
-			 </div>
-		 );
-	 }
-	 if (store.selectedSchemaType === 'act') {
-		 return (
-			 <div>
-				 <p>
-					 <b>actBox: </b>
-					 {(props.message as ActMessageItem).actBox}
-				 </p>
-				 <p>
-					 <b>fullServiceName: </b>
-					 {(props.message as ActMessageItem).fullServiceName}
-				 </p>
-				 <p>
-					 <b>methodName: </b>
-					 {(props.message as ActMessageItem).methodName}
-				 </p>
-			 </div>
-		 );
-	 }
-	 return <div></div>;
+	const store = useStore();
+	if (store.selectedSchemaType === 'parsed-message') {
+		return (
+			<div className='messageEntity'>
+				<p>
+					<b>session: </b>
+					{(props.message as ParsedMessageItem).sessionId}
+				</p>
+				<p>
+					<b>dictionary: </b>
+					{(props.message as ParsedMessageItem).dictionary}
+				</p>
+				<p>
+					<b>messageType: </b>
+					{(props.message as ParsedMessageItem).messageType}
+				</p>
+			</div>
+		);
+	}
+	if (store.selectedSchemaType === 'act') {
+		return (
+			<div>
+				<p>
+					<b>actBox: </b>
+					{(props.message as ActMessageItem).actBox}
+				</p>
+				<p>
+					<b>fullServiceName: </b>
+					{(props.message as ActMessageItem).fullServiceName}
+				</p>
+				<p>
+					<b>methodName: </b>
+					{(props.message as ActMessageItem).methodName}
+				</p>
+			</div>
+		);
+	}
+	return <div></div>;
 };
 
+interface MessageItemProps extends EditMessageProps {
+	index: number;
+	message: ParsedMessageItem | ActMessageItem;
+	selectMessage: Function;
+	setDelay: Function;
+	deleteMessage: Function;
+}
+
 const MessageItem = ({
-	 index,
-	 message,
-	 indicators,
-	 selectMessage,
-	 setDelay,
-	 deleteMessage,
-	 editMessageMode,
-	 editedMessageIndex,
+	index,
+	message,
+	indicators,
+	selectMessage,
+	setDelay,
+	deleteMessage,
+	editMessageMode,
+	editedMessageIndex,
 }: MessageItemProps) => (
-	 <div className='messageCard'>
-		 <div
-			 onClick={() => {
-				 selectMessage(index);
-			 }}>
-			 <MessageEntity message={message} />
-			 <p>
-				 <b>delay: </b>
-				 {editMessageMode && editedMessageIndex === index ? (
-					 <input
-						 className='delayInput'
-						 type='number'
-						 defaultValue={message.delay || 0}
-						 onChange={e => {
-							 setDelay(e.target.value as unknown as number);
-						 }}></input>
-				 ) : (
-					 message.delay
-				 )}
-				 ms
-			 </p>
-		 </div>
-		 <MessageCardControls
-			 deleteMessage={deleteMessage}
-			 editMessageMode={editMessageMode}
-			 index={index}
-			 indicator={indicators[index]}
-		 />
-	 </div>
+	<div className='messageCard'>
+		<div
+			onClick={() => {
+				selectMessage(index);
+			}}>
+			<MessageEntity message={message} />
+			<p>
+				<b>delay: </b>
+				{editMessageMode && editedMessageIndex === index ? (
+					<input
+						className='delayInput'
+						type='number'
+						defaultValue={message.delay || 0}
+						onChange={e => {
+							setDelay(e.target.value as unknown as number);
+						}}></input>
+				) : (
+					message.delay
+				)}
+				ms
+			</p>
+		</div>
+		<MessageCardControls
+			deleteMessage={deleteMessage}
+			editMessageMode={editMessageMode}
+			index={index}
+			indicator={indicators[index]}
+		/>
+	</div>
 );
 
+interface MessageCardControlsProps {
+	deleteMessage: Function;
+	editMessageMode: boolean;
+	index: number;
+	indicator: Indicator;
+}
+
 const MessageCardControls = ({
-	 deleteMessage,
-	 editMessageMode,
-	 index,
-	 indicator,
+	deleteMessage,
+	editMessageMode,
+	index,
+	indicator,
 }: MessageCardControlsProps) => (
-	 <div className='cardItems'>
-		 <button
-			 disabled={editMessageMode}
-			 className='deleteButton'
-			 onClick={() => {
-				 deleteMessage(index);
-			 }}>
-			 x
-		 </button>
-		 <Indicator className={indicator} />
-	 </div>
+	<div className='cardItems'>
+		<button
+			disabled={editMessageMode}
+			className='deleteButton'
+			onClick={() => {
+				deleteMessage(index);
+			}}>
+			x
+		</button>
+		<Indicator className={indicator} />
+	</div>
 );
 
 const Indicator = (props: { className: Indicator }) => (
-	 <div>
-		 <button className={props.className.toString()}></button>
-	 </div>
+	<div>
+		<button className={props.className.toString()}></button>
+	</div>
 );
 
+interface MessageListProps extends EditMessageProps {
+	messages: ParsedMessageItem[] | ActMessageItem[];
+}
+
 const MessageList = ({
-	 indicators,
-	 messages,
-	 editMessageMode,
-	 editedMessageIndex,
+	indicators,
+	messages,
+	editMessageMode,
+	editedMessageIndex,
 }: MessageListProps) => {
-	 const store = useStore();
+	const store = useStore();
 
-	 const deleteMessage = (index: number) => {
-		 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-		 const newArray: any[] = [];
+	const deleteMessage = (index: number) => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const newArray: any[] = [];
 
-		 const tmpIndicators: Indicator[] = store.deleteIndicator(index);
-		 store
-			 .getCurrentMessagesArray()
-			 .forEach((item: ParsedMessageItem | ActMessageItem, i: number) => {
-				 if (i !== index) {
-					 newArray.push(item);
-				 }
-			 });
-		 store.clearParsedMessages();
-		 newArray.forEach((mess: ParsedMessageItem | ActMessageItem, i) => {
-			 store.addParsedMessage(mess, tmpIndicators[i]);
-		 });
-	 };
+		const tmpIndicators: Indicator[] = store.deleteIndicator(index);
+		store
+			.getCurrentMessagesArray()
+			.forEach((item: ParsedMessageItem | ActMessageItem, i: number) => {
+				if (i !== index) {
+					newArray.push(item);
+				}
+			});
+		store.clearParsedMessages();
+		newArray.forEach((mess: ParsedMessageItem | ActMessageItem, i) => {
+			store.addParsedMessage(mess, tmpIndicators[i]);
+		});
+	};
 
-	 const setDelay = (delay: number) => {
-		 store.setEditedMessageSendDelay(delay);
-	 };
+	const setDelay = (delay: number) => {
+		store.setEditedMessageSendDelay(delay);
+	};
 
-	 return (
-		 <div className='scrolledBlock'>
-			 <ul>
-				 <li>
-					 {editMessageMode ? (
-						 <div
-							 className={'normalNewMessage'}
-							 onClick={() => {
-								 store.setEditMessageMode(false);
-							 }}>
-							 New Message
-						 </div>
-					 ) : null}
-				 </li>
+	return (
+		<div className='scrolledBlock'>
+			<ul>
+				<li>
+					{editMessageMode ? (
+						<div
+							className={'normalNewMessage'}
+							onClick={() => {
+								store.setEditMessageMode(false);
+							}}>
+							New Message
+						</div>
+					) : null}
+				</li>
 
-				 {((messages as ParsedMessageItem[]) || (messages as ActMessageItem[])).map(
-					 (item: ParsedMessageItem | ActMessageItem, index: number) => (
-						 <li key={nanoid()} className='messageItem'>
-							 <MessageItem
-								 index={index}
-								 message={item}
-								 indicators={indicators}
-								 editMessageMode={editMessageMode}
-								 editedMessageIndex={editedMessageIndex}
-								 selectMessage={store.selectMessage}
-								 deleteMessage={deleteMessage}
-								 setDelay={setDelay}
-							 />
-						 </li>
-					 ),
-				 )}
-			 </ul>
-		 </div>
-	 );
+				{((messages as ParsedMessageItem[]) || (messages as ActMessageItem[])).map(
+					(item: ParsedMessageItem | ActMessageItem, index: number) => (
+						<li key={nanoid()} className='messageItem'>
+							<MessageItem
+								index={index}
+								message={item}
+								indicators={indicators}
+								editMessageMode={editMessageMode}
+								editedMessageIndex={editedMessageIndex}
+								selectMessage={store.selectMessage}
+								deleteMessage={deleteMessage}
+								setDelay={setDelay}
+							/>
+						</li>
+					),
+				)}
+			</ul>
+		</div>
+	);
 };
 
 const EmbeddedEditor = (props: { schema: string; object: string }) => {
-	 const url = `http://localhost:3000?schema=${props.schema}&${
-		 props.object == null ? '' : `object=${props.object}`
-	 }&editorMode=dictionaryEditor&embedded=true`;
-	 return (
-		 <div className='scrolledBlock'>
-			 <iframe className='embeddedEditor' src={url}></iframe>
-		 </div>
-	 );
+	const url = `http://localhost:3000?schema=${props.schema}&${
+		props.object == null ? '' : `object=${props.object}`
+	}&editorMode=dictionaryEditor&embedded=true`;
+	return (
+		<div className='scrolledBlock'>
+			<iframe className='embeddedEditor' src={url}></iframe>
+		</div>
+	);
 };
 
-const MessageEditArea = ({
-	 editMessageMode,
-	 indicators,
-	 messages,
-	 editedMessageIndex,
-	 eventsPanelArea,
-}: MessageEditAreaProps) => {
-	 const store = useStore();
-	 return (
-		 <div className='messageEditArea'>
-			 {store.selectedSchemaType === 'parsed-message' ? (
-				 <SplitView panelArea={eventsPanelArea} onPanelAreaChange={store.setPanelArea}>
-					 <SplitViewPane>
-						 <MessageList
-							 messages={messages}
-							 indicators={indicators}
-							 editMessageMode={editMessageMode}
-							 editedMessageIndex={editedMessageIndex}
-						 />
-					 </SplitViewPane>
+interface MessageEditAreaProps extends EditMessageProps {
+	messages: ParsedMessageItem[] | ActMessageItem[];
+	eventsPanelArea: number;
+	object: string | null;
+}
 
-					 <SplitViewPane>
-						 <EmbeddedEditor
-							 schema='schema-qa'
-							 object={store.selectedDictionaryName || ''}
-						 />
-					 </SplitViewPane>
-				 </SplitView>
-			 ) : (
-				 <MessageList
-					 messages={messages}
-					 indicators={indicators}
-					 editMessageMode={editMessageMode}
-					 editedMessageIndex={editedMessageIndex}
-				 />
-			 )}
-		 </div>
-	 );
+const MessageEditArea = ({
+	editMessageMode,
+	indicators,
+	messages,
+	editedMessageIndex,
+	eventsPanelArea,
+	object,
+}: MessageEditAreaProps) => {
+	const store = useStore();
+	return (
+		<div className='messageEditArea'>
+			{store.selectedSchemaType === 'parsed-message' ? (
+				<SplitView panelArea={eventsPanelArea} onPanelAreaChange={store.setPanelArea}>
+					<SplitViewPane>
+						<MessageList
+							messages={messages}
+							indicators={indicators}
+							editMessageMode={editMessageMode}
+							editedMessageIndex={editedMessageIndex}
+						/>
+					</SplitViewPane>
+
+					<SplitViewPane>
+						<EmbeddedEditor schema='schema-qa' object={object || ''} />
+					</SplitViewPane>
+				</SplitView>
+			) : (
+				<MessageList
+					messages={messages}
+					indicators={indicators}
+					editMessageMode={editMessageMode}
+					editedMessageIndex={editedMessageIndex}
+				/>
+			)}
+		</div>
+	);
 };
 
 observer(MessageEditArea);
