93478eefd53ad4704ace4542631cae48b6219059
MariaMakii <themarymakii@gmail.com>
2021-09-22 17:29:19 +0300

    Reduced code in ResultMonacoEditor.tsx

--- src/components/ResultMonacoEditor.tsx
+++ src/components/ResultMonacoEditor.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
-import Editor, {
-	monaco,
-	EditorDidMount,
-	Monaco,
-} from '@monaco-editor/react';
+import Editor, { monaco, Monaco } from '@monaco-editor/react';
 
 const ResultMonacoEditor = (props: { value: string }) => {
 	const monacoEditor = React.useRef<Monaco>();
-	const handleEditorDidMount: EditorDidMount = (fn, editor) => {
-		editor.updateOptions({
-			minimap: { enabled: false },
-			readOnly: true,
-			lineNumbers: 'off',
-			wordWrap: 'on',
-		});
-	};
 
-	React.useEffect(() => {
-		monaco.init().then((_monaco: Monaco) => {
-			monacoEditor.current = _monaco;
-			monacoEditor.current.languages.json.jsonDefaults.setDiagnosticsOptions({
-				validate: false,
-			});
+	monaco.init().then((_monaco: Monaco) => {
+		monacoEditor.current = _monaco;
+		monacoEditor.current.languages.json.jsonDefaults.setDiagnosticsOptions({
+			validate: false,
 		});
-	}, []);
+	});
 
 	return (
 		<Editor
@@ -31,7 +17,12 @@ const ResultMonacoEditor = (props: { value: string }) => {
 			width={'100%'}
 			language='json'
 			value={props.value}
-			editorDidMount={handleEditorDidMount}></Editor>
+			options={{
+				minimap: { enabled: false },
+				readOnly: true,
+				lineNumbers: 'off',
+				wordWrap: 'on',
+			}}></Editor>
 	);
 };
 
