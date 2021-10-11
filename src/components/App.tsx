8ce18d5d6bab8214c2735231911d0be9dd59f350
molotgor <molotgog@gmail.com>
2021-10-11 17:08:58 +0400

    added copying editor files in build process

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -18,6 +18,7 @@ import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 import { observer } from 'mobx-react-lite';
 import { Tab, Tabs } from '@material-ui/core';
+import loader from '@monaco-editor/loader';
 import Result from './Result';
 import Button from './Button';
 import '../styles/root.scss';
@@ -41,6 +42,10 @@ const App = () => {
 	const [response, setResponse] = React.useState<MessageSendingResponse | null>(null);
 
 	const messageEditorRef = React.useRef<MessageEditorMethods>(null);
+
+	React.useEffect(() => {
+		loader.config({ paths: { vs: 'vs' } });
+	}, []);
 
 	const sendMessage = async () => {
 		if (messageEditorRef.current) {
--- webpack/initEditor.js
+++ webpack/initEditor.js
@@ -5,15 +5,15 @@ const dir = './public/vs';
 fs.emptyDirSync(dir);
 
 Promise.all([
-    fs.copy('./node_modules/monaco-editor/min/vs/base', './public/vs/base'),
-    fs.copy('./node_modules/monaco-editor/min/vs/editor/', './public/vs/editor', {
+    fs.copy('./node_modules/monaco-editor/min/vs/base', './resources/vs/base'),
+    fs.copy('./node_modules/monaco-editor/min/vs/editor/', './resources/vs/editor', {
         filter: (src) => {
             const files = ['editor.main.css', 'editor.main.js', 'editor.main.nls.js'];
             return !/\.(js|css)?$/.test(src) || files.some(file => src.includes(file));
         },
     }),
-    fs.copy('./node_modules/monaco-editor/min/vs/language/json', './public/vs/language/json'),
-    fs.copy('./node_modules/monaco-editor/min/vs/loader.js', './public/vs/loader.js'),
+    fs.copy('./node_modules/monaco-editor/min/vs/language/json', './resources/vs/language/json'),
+    fs.copy('./node_modules/monaco-editor/min/vs/loader.js', './resources/vs/loader.js'),
 ]).catch(() => {
     throw new Error('Failed to copy editor files');
 });
