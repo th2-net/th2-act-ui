6af25040612976f4ceacd488ad3bccc5b72c51a7
Igor Razvodov <razvodovigor.dev@gmail.com>
2021-10-19 11:48:58 +0300

    Delete duplicate plugin, fix indentation

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -24,68 +24,70 @@ const { appSrc, appPath } = require('./paths');
 const api_env = process.env.API_ENV || 'http';
 
 module.exports = {
-    resolve: {
-        extensions: ['.ts', '.tsx', '.scss', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(ts|tsx)$/,
-                loader: "babel-loader",
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg|jpg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'resources/',
-                    },
-                }]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.EnvironmentPlugin({
-            API_ENV: api_env
-        }),
-        new HtmlWebPackPlugin({
-            title: 'Message-sender-ui',
-            template: path.resolve(appSrc, 'index.html'),
-            favicon: false,
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: './node_modules/monaco-editor/min/vs/base',
-                    to: './resources/vs/base',
-                },
-                {
-                    from: './node_modules/monaco-editor/min/vs/editor',
-                    to: './resources/vs/editor',
-                    filter: (src) => {
-                        const files = ['editor.main.css', 'editor.main.js', 'editor.main.nls.js'];
-                        return !/\.(js|css)?$/.test(src) || files.some(file => src.includes(file));
-                    }
-                },
-                {
-                    from: './node_modules/monaco-editor/min/vs/language/json',
-                    to: './resources/vs/language/json',
-                },
-                {
-                    from: './node_modules/monaco-editor/min/vs/basic-languages/xml',
-                    to: './resources/vs/basic-languages/xml',
-                },
-                {
-                    from: './node_modules/monaco-editor/min/vs/loader.js',
-                    to: './resources/vs/loader.js',
-                },
-            ],
-        }),
-    ],
+	resolve: {
+		extensions: ['.ts', '.tsx', '.scss', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(ts|tsx)$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg|jpg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'resources/',
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new webpack.EnvironmentPlugin({
+			API_ENV: api_env,
+		}),
+		new HtmlWebPackPlugin({
+			title: 'Message-sender-ui',
+			template: path.resolve(appSrc, 'index.html'),
+			favicon: false,
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: './node_modules/monaco-editor/min/vs/base',
+					to: './resources/vs/base',
+				},
+				{
+					from: './node_modules/monaco-editor/min/vs/editor',
+					to: './resources/vs/editor',
+					filter: src => {
+						const files = ['editor.main.css', 'editor.main.js', 'editor.main.nls.js'];
+						return !/\.(js|css)?$/.test(src) || files.some(file => src.includes(file));
+					},
+				},
+				{
+					from: './node_modules/monaco-editor/min/vs/language/json',
+					to: './resources/vs/language/json',
+				},
+				{
+					from: './node_modules/monaco-editor/min/vs/basic-languages/xml',
+					to: './resources/vs/basic-languages/xml',
+				},
+				{
+					from: './node_modules/monaco-editor/min/vs/loader.js',
+					to: './resources/vs/loader.js',
+				},
+			],
+		}),
+	],
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -18,7 +18,6 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const { appSrc } = require('./paths');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = webpackMerge(commonConfig, {
 	output: {
@@ -30,89 +29,59 @@ module.exports = webpackMerge(commonConfig, {
 	devServer: {
 		watchOptions: {
 			poll: true,
-			ignored: [/node_modules/, 'src/__tests__/']
+			ignored: [/node_modules/, 'src/__tests__/'],
 		},
 		compress: true,
 		port: 9001,
-		host: "0.0.0.0",
+		host: '0.0.0.0',
 		historyApiFallback: true,
 		hot: true,
 		proxy: {
-            '/backend': {
-                target: 'http://th2-qa:30000/th2-commonv3/act-ui/backend/',
-                pathRewrite: { '^/backend': '' },
-                secure:false,
-                changeOrigin:true,
-            },
-            '/editor2': {
+			'/backend': {
+				target: 'http://th2-qa:30000/th2-commonv3/act-ui/backend/',
+				pathRewrite: { '^/backend': '' },
+				secure: false,
+				changeOrigin: true,
+			},
+			'/editor2': {
 				target: 'http://th2-qa:30000',
-                secure: false,
-                changeOrigin: true,
-            },
-        },
+				secure: false,
+				changeOrigin: true,
+			},
+		},
 	},
 	module: {
 		rules: [
 			{
 				test: /\.(ts|tsx)$/,
 				enforce: 'pre',
-				use: [{
-					options: {
-						eslintPath: require.resolve('eslint'),
-						failOnError: false,
-						cache: false,
-						quite: true,
-						formatter: require('eslint-formatter-pretty'),
+				use: [
+					{
+						options: {
+							eslintPath: require.resolve('eslint'),
+							failOnError: false,
+							cache: false,
+							quite: true,
+							formatter: require('eslint-formatter-pretty'),
+						},
+						loader: require.resolve('eslint-loader'),
 					},
-					loader: require.resolve('eslint-loader'),
-				}],
+				],
 				exclude: /node_modules/,
 			},
 			{
 				test: /\.scss$/,
 				exclude: /node_modules/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				].filter(loader => loader)
+				use: ['style-loader', 'css-loader', 'sass-loader'].filter(loader => loader),
 			},
-		]
+		],
 	},
 	plugins: [
 		new ForkTsCheckerWebpackPlugin({
 			eslint: {
 				files: './src/**/*{ts,tsx,js,jsx}',
 				enabled: true,
-			}
+			},
 		}),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: './node_modules/monaco-editor/min/vs/base',
-                    to: './resources/vs/base',
-                },
-                {
-                    from: './node_modules/monaco-editor/min/vs/editor',
-                    to: './resources/vs/editor',
-                    filter: (src) => {
-                        const files = ['editor.main.css', 'editor.main.js', 'editor.main.nls.js'];
-                        return !/\.(js|css)?$/.test(src) || files.some(file => src.includes(file));
-                    }
-                },
-                {
-                    from: './node_modules/monaco-editor/min/vs/language/json',
-                    to: './resources/vs/language/json',
-                },
-                {
-                    from: './node_modules/monaco-editor/min/vs/basic-languages/xml',
-                    to: './resources/vs/basic-languages/xml',
-                },
-                {
-                    from: './node_modules/monaco-editor/min/vs/loader.js',
-                    to: './resources/vs/loader.js',
-                },
-            ],
-        }),
-	]
+	],
 });
