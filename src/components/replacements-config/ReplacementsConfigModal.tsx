3499210767f45b419df0dbde501eeb682740d5eb
TheodorSchmidt <bogdan_zakharov_99@mail.ru>
2022-06-16 12:51:28 +0400

    change index in the replacementconfig path to message name

--- src/components/replacements-config/ReplacementsConfigModal.tsx
+++ src/components/replacements-config/ReplacementsConfigModal.tsx
@@ -77,14 +77,19 @@ const ReplacementsConfigModal = ({ open, onClose }: Props) => {
 		let paths: string[] = ['$datetime'];
 
 		replayList.forEach((replayItem, index) => {
-			paths.push(`/${index}`);
+			let name = '';
+			if (replayItem.name) {
+				name = replayItem.name;
+			} else {
+				name = `untitled${index}`;
+			}
+			paths.push(`/${name}`);
 
 			let bodyPaths: string[];
-
 			try {
 				bodyPaths = Object.keys(jsm.parse(replayItem.message).pointers)
 					.filter(x => x)
-					.map(x => `/${index}/body${x}`);
+					.map(x => `/${name}/body${x}`);
 			} catch {
 				bodyPaths = [];
 			}
@@ -94,7 +99,7 @@ const ReplacementsConfigModal = ({ open, onClose }: Props) => {
 			if (replayItem.result.response?.code === 200) {
 				const resultPaths = Object.keys(jsm.parse(replayItem.result.response.message).pointers)
 					.filter(x => x)
-					.map(x => `/${index}/response${x}`);
+					.map(x => `/${name}/response${x}`);
 
 				paths = [...paths, ...resultPaths];
 			}
